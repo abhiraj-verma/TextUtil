c25072a2b4a7e32fb6b9904f20369503ad5da2b0
Sumitbhatia <sumitbhatia2703@gmail.com>
2025-03-05 09:28:11 +0530

    Added initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import TextForm from './Components/TextForm';
+import Navbar from './Components/Navbar';
+//import About from './Components/About';
+import { useState } from 'react';
+import Alert from './Components/Alert';
+import About from './Components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] =  useState('light');
+  const [alert, setAlert] = useState(null);
+  const [isBlackButtonChecked, setBlackButton] = useState(false);
+  const [isBluishButtonChecked, setBlueButton] = useState(false);
+ 
+
+  const toggleMode = (color, type)=> {
+    //console.log("Background color: " + document.body.style.backgroundColor + " | passed " + color + " : " + type);
+    if (mode === 'light' || 
+      (document.body.style.backgroundColor==='black' ^ color==='black')) {
+      setMode('dark');
+      document.body.style.backgroundColor =  color;
+      showAlert(type + " mode has been enabled", "success");
+      if (color === 'black') {
+        setBlackButton(true);
+        setBlueButton(false);
+      } else {
+        setBlackButton(false);
+        setBlueButton(true);
+      }
+      document.title = "Text Util - " + type + " Mode";
+      // setInterval(()=>{document.title = "Text Util is amazing"}, 2000);
+      // setInterval(()=>{document.title = "Text Util Download"}, 1500);
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      setBlackButton(false);
+      setBlueButton(false);
+      document.title = "Text Util - Light Mode";
+    }
+  }
+
+  const showAlert = (message , type) => {
+    setAlert({
+      message : message,
+      type : type
+    })
+    setTimeout(()=> {
+      setAlert(null);
+    }, 1500)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Router>
+   <Navbar title="TextUtil" about= "About" mode={mode} toggleMode={toggleMode} 
+   blueButton={isBluishButtonChecked} blackButton={isBlackButtonChecked}/>
+   <Alert alert={alert}/>
+   <div className="container my-3">
+    <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/"
+           element={<TextForm showAlert={showAlert} header="Enter the text below to analyze" mode={mode}/>}/>   
+    </Routes>
+   </div>
+   </Router>
+   </>
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function FormText(props) {
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "success");
+  };
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase", "success");
+  };
+
+  const countActualWord = (word)=> {
+    return word.split(/\s+/).filter(Boolean).length;
+  }
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container" style={{color : props.mode==='dark' ? 'white':'#042743'}}>
+        <h1>{props.header}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            style={{backgroundColor : props.mode === 'dark' ? '#042743' : 'white', 
+              color : props.mode==='dark' ? 'white':'#042743'
+            }}
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to Lowercase
+        </button>
+      </div>
+      <div className="container my-2" style={{color : props.mode==='dark' ? 'white':'#042743'}}>
+        <h1>Your Text Summary</h1>
+        <p>
+          {countActualWord(text)} Words and {text.length} Characters
+        </p>
+        <p>{0.08 * countActualWord(text)} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0? text : "Enter something in above box to preview it"}</p>
+      </div>
+    </>
+  );
+}
